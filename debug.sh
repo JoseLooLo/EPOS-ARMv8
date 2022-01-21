66a53d8423dba6e6709978b6c5876d965295d5ff
JoseLooLo <joseloolo@hotmail.com>
2022-01-21 10:49:03 -0300

    switch context load

--- debug.sh
+++ debug.sh
@@ -8,17 +8,18 @@ konsole -e aarch64-elf-gdb -ex "target remote:1236" -ex "set confirm off" \
 -ex "add-symbol-file /home/josef/Desktop/INE5424/img/system_raspberry_pi3" \
 -ex "add-symbol-file /home/josef/Desktop/INE5424/img/init_raspberry_pi3" \
 -ex "b load() const volatile" \
--ex "b *0x80080" \
--ex "b *0x80100" \
--ex "b *0x80180" \
--ex "b *0x80200" \
--ex "b *0x80280" \
--ex "b *0x80300" \
--ex "b *0x80380" \
--ex "b *0x80400" \
+-ex "b switch_context(EPOS::S::CPU::Context**, EPOS::S::CPU::Context*)" \
 -ex "c" \
 -ex "layout split"'
 
+# -ex "b *0x80080" \
+# -ex "b *0x80100" \
+# -ex "b *0x80180" \
+# -ex "b *0x80200" \
+# -ex "b *0x80280" \
+# -ex "b *0x80300" \
+# -ex "b *0x80380" \
+# -ex "b *0x80400" \
 
 #-ex "b *0x0080000" \
 #EPOS::S::Setup::Setup
--- src/architecture/armv8/armv8_cpu.cc
+++ src/architecture/armv8/armv8_cpu.cc
@@ -114,44 +114,6 @@ void CPU::Context::load() const volatile
         ASM("eret");
 }
 
-// void CPU::Context::load() const volatile
-// {
-//     ASM("mov sp, %0     \n"
-//         "isb            \n" : : "r"(this)); // serialize the pipeline so that SP gets updated before the pop
-
-//     ASM("add sp, sp, #16    \n");       // skip usp, ulr
-
-//     //Load flags
-//     ASM("ldr x20, [sp], #8\n");
-//     x20_to_state();
-
-//     //Load regs
-//     ASM("ldr    x0, [sp], #8            \n"
-// 		"ldp	x1, x2, [sp], #16       \n"
-// 		"ldp	x3, x4, [sp], #16       \n"
-// 		"ldp	x5, x6, [sp], #16       \n"
-// 		"ldp	x7, x8, [sp], #16       \n"
-// 		"ldp	x9, x10, [sp], #16      \n"
-// 		"ldp	x11, x12, [sp], #16     \n"
-// 		"ldp	x13, x14, [sp], #16     \n"
-// 		"ldp	x15, x16, [sp], #16     \n"
-// 		"ldp	x17, x18, [sp], #16     \n"
-// 		"ldp	x19, x20, [sp], #16     \n"
-// 		"ldp	x21, x22, [sp], #16     \n"
-// 		"ldp	x23, x24, [sp], #16     \n"
-// 		"ldp	x25, x26, [sp], #16     \n"
-// 		"ldp	x27, x28, [sp], #16     \n"
-// 		"ldp	x29, x30, [sp], #16     \n"
-//         //lr
-//         "ldr  x30, [sp], #8             \n");
-//         //load PC ?
-//         ASM(
-//         "ldr  x30, [sp], #8             \n"
-//         "msr elr_el1, x30               \n"
-//         "ldr  x30, [sp, #-16]           \n"
-//         "eret");
-// }
-
 // This function assumes A[T]PCS (i.e. "o" is in r0/a0 and "n" is in r1/a1)
 void CPU::switch_context(Context ** o, Context * n)
 {
@@ -212,17 +174,9 @@ void CPU::switch_context(Context ** o, Context * n)
 
     //Load flags
     ASM("ldr x20, [sp], #8\n");
-    // x20_to_state();
-
-    ASM("str x20, [sp, #-8]!\n");
-    state_2_x20();
     x20_to_state();
-    ASM(
-    "ldr x20, [sp]\n"
-    "add sp, sp, 8\n");
 
     //Load regs
-    //This is probably a bug
     ASM("ldr  x0, [sp], #8              \n"
 		"ldp	x1, x2, [sp], #16       \n"
 		"ldp	x3, x4, [sp], #16       \n"
@@ -241,13 +195,16 @@ void CPU::switch_context(Context ** o, Context * n)
 		"ldp	x29, x30, [sp], #16     \n"
         //lr
         "ldr  x30, [sp], #8             \n");
-        //load PC ?
+        //load PC
         ASM(
         "ldr  x30, [sp], #8             \n"
         "msr elr_el1, x30               \n"
-        "ldr  x30, [sp, #-16]           \n"
-        "eret\n"
-        ".ret: br x30\n");
+        "ldr  x30, [sp, #-16]           \n");
+        //Run the new context
+        ASM("eret\n");
+
+        //Old context return address
+        ASM(".ret: br x30\n");
 }
 
 __END_SYS
